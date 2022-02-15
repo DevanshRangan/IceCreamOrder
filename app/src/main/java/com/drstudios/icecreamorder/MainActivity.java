a23efa1729e0657ca1fc52ff21f31158c3a0c92d
devansh <devanshrangan04@gmail.com>
2022-02-15 16:19:03 +0530

    sp

--- app/src/main/java/com/drstudios/icecreamorder/MainActivity.java
+++ app/src/main/java/com/drstudios/icecreamorder/MainActivity.java
@@ -3,6 +3,7 @@ package com.drstudios.icecreamorder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -23,7 +24,9 @@ public class MainActivity extends AppCompatActivity {
     int quantityText = 0;
     double totalCost = 0;
     DecimalFormat decimalFormat;
+    public static final String prefName = "SharedPrefsFile";
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +41,27 @@ public class MainActivity extends AppCompatActivity {
         totalCostTextView = findViewById(R.id.totalCostTextView);
         decimalFormat = new DecimalFormat("0.00");
 
+        @SuppressLint("CommitPrefEdits") SharedPreferences.Editor editor = getSharedPreferences(prefName, MODE_PRIVATE).edit();
+
+        SharedPreferences preferences = getSharedPreferences(prefName, MODE_PRIVATE);
+        String selectedRadio = preferences.getString("SelectedRadio", "null");
+        if (selectedRadio.equals("Cup")){
+            cup.setSelected(true);
+            calculate.setEnabled(true);
+        } else if (selectedRadio.equals("Cone")){
+            cone.setSelected(true);
+            calculate.setEnabled(true);
+        }
+
+        int getQuantity = preferences.getInt("Quantity", 0);
+        quantity.setText(Integer.toString(getQuantity));
+        quantityText = getQuantity;
+
+        String total = preferences.getString("Total", "$0.00");
+        totalCostTextView.setText(total);
+
+
+
 
 
         cup.setOnClickListener(new View.OnClickListener() {
@@ -45,6 +69,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 calculate.setEnabled(true);
                 iceCreamImage.setImageResource(R.drawable.ice_cream_cup);
+                editor.putString("SelectedRadio", "Cup");
+                editor.apply();
             }
         });
 
@@ -53,6 +79,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 calculate.setEnabled(true);
                 iceCreamImage.setImageResource(R.drawable.ice_cream_cone);
+                editor.putString("SelectedRadio", "Cone");
+                editor.apply();
             }
         });
 
@@ -61,6 +89,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 quantityText ++;
                 quantity.setText(Integer.toString(quantityText));
+                editor.putInt("Quantity", quantityText);
+                editor.apply();
             }
         });
 
@@ -71,6 +101,8 @@ public class MainActivity extends AppCompatActivity {
                 if (!(quantityText <= 0)){
                     quantityText --;
                     quantity.setText(Integer.toString(quantityText));
+                    editor.putInt("Quantity", quantityText);
+                    editor.apply();
                 } else
                     Toast.makeText(getApplicationContext(), "Quantity cannot be less than 0", Toast.LENGTH_SHORT).show();
             }
@@ -83,9 +115,13 @@ public class MainActivity extends AppCompatActivity {
                 if (cup.isChecked()){
                     totalCost = 3.49 * quantityText;
                     totalCostTextView.setText("$" + decimalFormat.format(totalCost));
+                    editor.putString("Total", totalCostTextView.getText().toString());
+                    editor.apply();
                 } else {
                     totalCost = 3.99 * quantityText;
                     totalCostTextView.setText("$" + decimalFormat.format(totalCost));
+                    editor.putString("Total", totalCostTextView.getText().toString());
+                    editor.apply();
                 }
             }
         });
